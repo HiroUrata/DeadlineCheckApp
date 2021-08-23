9fc5ddaf00f3f464684dbfd3506432579cec03ba
HiroUrata <1712026@i.softbank.jp>
2021-08-23 16:31:25 +0900

    TabBarController カスタムを追加

--- DeadlineCheckApp/Controller/TabBarController.swift
+++ DeadlineCheckApp/Controller/TabBarController.swift
@@ -8,7 +8,9 @@
 import UIKit
 
 class TabBarController: UITabBarController {
-
+    
+    var howHiddenBool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,13 +20,31 @@ class TabBarController: UITabBarController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        self.navigationItem.rightBarButtonItems = [UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(showSearchAlert)),
+        tabBar.barTintColor =  UIColor(red: 1.0, green: 0.40, blue: 0.51, alpha: 1.0)
+        tabBar.tintColor = .white
+        tabBar.layer.masksToBounds = true
+        tabBar.layer.cornerRadius = 20.0
+        
+
+        navigationItem.rightBarButtonItems = [UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(showSearchAlert)),
                                                    UIBarButtonItem(title: "Today", style: .done, target: self, action: #selector(showTodayList))]
         
     }
     
     @objc func showSearchAlert(){
-
+        
+        if howHiddenBool == true {
+            
+            UITabBar.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut, animations: {self.tabBar.frame.origin.y = self.tabBar.frame.origin.y + self.tabBar.frame.size.height}, completion: nil)
+            howHiddenBool = false
+            
+        }else if howHiddenBool == false{
+            
+            UITabBar.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut, animations: {self.tabBar.frame.origin.y = self.tabBar.frame.origin.y - self.tabBar.frame.size.height}, completion: nil)
+            howHiddenBool = true
+            
+        }
+        
 
 
     }
